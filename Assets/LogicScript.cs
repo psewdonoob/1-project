1f40ce05e8ccbe4f124448e1eaf1e1329cdf7e0f
nik5 <nikola_sarkozi@mail.ru>
2025-01-01 15:46:59 +0400

    Add: GameOver screen with logic; Change: distance between Rocks

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
     public int playerScore = 0;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
@@ -14,14 +16,24 @@ public class LogicScript : MonoBehaviour
         scoreText.text = playerScore.ToString();
     }
 
-    /*
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gameOverScreen.SetActive(false);
     }
-
+    /*
     // Update is called once per frame
     void Update()
     {
--- Assets/PlaneScript.cs
+++ Assets/PlaneScript.cs
@@ -4,20 +4,29 @@ public class PlaneScript : MonoBehaviour
 {
     public Rigidbody2D PlaneRigidbody;
     public float planeLiftStrength;
+    public LogicScript logic;
+    public bool isAlive;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+        isAlive = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isAlive)
         {
             PlaneRigidbody.linearVelocity = Vector2.up * planeLiftStrength;
-        }
-        
+        }        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        isAlive = false;
     }
 }
